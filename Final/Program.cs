6a4ec44a77dcaf41f6e84b97853653133a2fbe22
Denis Myagkov <tomich-84@mail.ru>
2022-12-24 15:56:30 +0300

    got array

--- Final/Program.cs
+++ Final/Program.cs
@@ -1,16 +1,27 @@
 ﻿Console.Clear();
-int number = GetNumberFromUser("Введите количество строк в массиве: ", "Ошибка ввода!");
+int userNumber = GetNumberFromUser("Введите количество строк в массиве: ", "Ошибка ввода!");
+string[] array = new string[userNumber];
+FillArray(userNumber, array);
 
 
+
+void FillArray(int number, string[] arr)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write("Введите строку: ");
+        arr[i] = Console.ReadLine();
+    }
+}
 
 int GetNumberFromUser(string message, string errorMessage)
 {
-    while(true)
+    while (true)
     {
-    Console.WriteLine(message);
-    if(int.TryParse(Console.ReadLine(), out int userNumber))
-    return userNumber;
-    else
-    Console.WriteLine(errorMessage);
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        else
+            Console.WriteLine(errorMessage);
     }
 }
